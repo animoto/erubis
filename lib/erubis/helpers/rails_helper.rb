e055d64a8e983896a7b9e105761bc9d8e55bba56
Erik Michaels-Ober <sferik@gmail.com>
2010-09-30 09:04:03 -0700

    Don't log Erubis::VERSION on each invocation

--- lib/erubis/helpers/rails_helper.rb
+++ lib/erubis/helpers/rails_helper.rb
@@ -347,7 +347,6 @@ else                                           ###  Rails 1.X
 
 end   ###
 
-
 ## finish
-ActionController::Base.new.logger.info "** Erubis #{::Erubis::VERSION}"
-$stdout.puts "** Erubis #{::Erubis::VERSION}" if rails22
+# ActionController::Base.new.logger.info "** Erubis #{::Erubis::VERSION}"
+# $stdout.puts "** Erubis #{::Erubis::VERSION}" if rails22
